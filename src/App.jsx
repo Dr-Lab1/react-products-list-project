852ea104fbc02fd1bf8534caeb72a295f037b63a
Jonathan Bahati <dr-lab1@MacBook-Air-de-Jonathan.local>
2024-07-07 12:48:52 +0100

    Configuration de la searchBar - 2024.07.07 at 12:49

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { useState } from 'react'
+import { SearchBar } from './components/forms/searchBar'
 
 function App() {
-  return 'Hello World'
+  return <div className="container col-6">
+    <NavBar />
+  </div>
+}
+
+function NavBar() {
+  return <div className="container">
+    <SearchBar placeholder="Rechercher..." />
+  </div>
 }
 
 export default App
---
+++ src/components/forms/searchBar.jsx
@@ -0,0 +1,5 @@
+export function SearchBar({value, placeholder, onSearch}) {
+    return <div className="my-3">
+        <input type="text" name="" id="" className="form-control" placeholder={placeholder} />
+    </div>
+}
