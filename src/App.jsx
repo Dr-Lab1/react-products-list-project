e6567a2108b3e71406457a1d683a84e5e825f971
Jonathan Bahati <dr-lab1@MacBook-Air-de-Jonathan.local>
2024-07-07 13:25:47 +0100

    Ajout de la liste des produits - 2024.07.07 at 13:27

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from 'react'
 import { SearchBar } from './components/forms/searchBar'
 import { CheckBox } from './components/forms/checkBox'
 
+const PRODUCTS = [  
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},  
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},  
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},  
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},  
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},  
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}  
+]
+
 function App() {
   return <div className="container col-6">
     <NavBar />
@@ -26,7 +35,7 @@ function ProductTable() {
     </thead>
 
     <tbody>
-      
+
     </tbody>
 
   </table>
